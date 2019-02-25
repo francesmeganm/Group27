e025ebf0c7c7753650380a4502dd73df01ad2c38
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-02-25 12:23:14 -0700

    Update UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,6 +1,33 @@
 public class UserInfo{
-	private String firstName = "";
-	private String lastName = "";
-	private double monthlyIncome = 0.0;
+	private String name = "";
+	private double monthlyIncome;
 	private double amountToSave = 0.0;
+	
+	public UserInfo(String n, double income, double save){
+		name = n;
+		monthlyIncome = setMonthlyIncome();
+		amountToSave = save; 
+	}
+	
+	public void setMonthlyIncome(double income){
+		if (income > 0){
+			monthlyIncome = income;
+		}
+		else{
+			monthlyIncome = 0.0;
+		}
+	}
+	
+	public double getMonthlyIncome(){
+		return monthlyIncome;
+	}
+	
+	public double getAmountToSave(){
+		return amountToSave;
+	}
+	
+	public double getName(){
+		return name;
+	}
+	
 }
