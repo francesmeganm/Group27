272346590e910f816f643d31b73954b87320b873
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-02-25 12:16:32 -0700

    Update Budget.java

--- Budget.java
+++ Budget.java
@@ -1,6 +1,14 @@
 public class Budget extends Expenses{
 	private double monthlyExpenses = 0.0;
 	private double monthylIncome = 0.0;
-	private double amountToSave = 0.0;
-	private double amountToSpend = 0.0;
+	
+	public Budget(expenses, income){
+		monthlyExpenses = super.getTotalExpenses();
+		monthlyIncome = super.getMonthlyIncome();
+	}
+		
+	public double remainingMoney(){
+		double remaining = monthlyIncome - monthlyExpenses;
+		return remaining;
+	}
 }
