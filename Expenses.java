afa2baaac5493fdd89e646d5baa66f7a31388d53
kathrynl13 <47461692+kathrynl13@users.noreply.github.com>
2019-02-25 12:25:08 -0700

    Update Expenses.java (#1)

--- Expenses.java
+++ Expenses.java
@@ -1,10 +1,101 @@
-public class Expenses{
+/**
+
+*/
+public class Expenses extends UserInfo{
 	private double rent = 0.0;
 	private double food = 0.0;
 	private double transportation = 0.0; 
 	private double utilities = 0.0;
-
-	public double totalExpenses(){
+	
+	/**
+	Defualt constructor- no parmerters 
+	*/
+	public Expenses(){
+	}
+	
+	/**
+	Constructor initializes all private instance variables 
+	*/
+	public Expenses(double rent, double food, double transporation, double utilities){
+		this.rent = rent;
+		this.food = food;
+		this.transporation = transporation;
+		this.utilities = utilties;
+	}
+	
+	/**
+	Method sets up the value of rent, with the condition that the rent is a non-negitive number
+	@param rentCost the cost of rent to be set 
+	*/
+	public void setRent(double rentCost){
+		if (rent >= 0){
+			this.rent = rentCost;
+		}
+	}
+	
+	/**
+	@return the value of the instance variable rent 
+	*/
+	pubic double getRent(){
+		return rent;
+	}
+	
+	/**
+	Method sets up the value of food, with the condition that the food is a non-negitive number
+	@param foodCost the cost of food to be set 
+	*/
+	public void setFood(double foodCost){
+		if (food >= 0){
+			this.food = foodCost;
+		}
+	}
+	
+	/**
+	@return the value of the instance variable food
+	*/
+	pubic double getFood(){
+		return food;
+	}
+	
+	/**
+	Method sets up the value of transporation, with the condition that the transporation is a non-negitive number
+	@param transporationCost the cost of transporation to be set 
+	*/
+	public void setTransporation(double transporationCost){
+		if (transporation >= 0){
+			this.transporation = transporationCost;
+		}
+	}
+	
+	/**
+	@return the value of the instance variable transporation
+	*/
+	pubic double getTransporation(){
+		return transporation;
+	}
+	
+	/**
+	Method sets up the value of utilities, with the condition that the utilities is a non-negitive number
+	@param utilitiesCost the cost of utilities to be set 
+	*/
+	public void setUtilities(double utilitiesCost){
+		if (utilities >= 0){
+			this.utilities = utilitiesCost;
+		}
+	}
+	
+	/**
+	@return the value of the instance variable utilities
+	*/
+	pubic double getUtilities(){
+		return utilities;
+	}
+	
+	/**
+	Method sums all the expenses up to get total expenses of the user for the month 
+	@return the total value of all the expenses
+	*/
+	public double getTotalExpenses(){
 		double total = rent + food + transportation + utilities;
 		return total;
 	}
