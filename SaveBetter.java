a5d0d253c6ba320ecc931ed3fa96e546da4eb43a
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-02-28 12:36:15 -0700

    Update SaveBetter.java

--- SaveBetter.java
+++ SaveBetter.java
@@ -1,6 +1,16 @@
 import java.util.HashMap;
 
-public class SaveBetter{
+public class SaveBetter extend MenuOptions{
+	String menu = "Update User Info		Update Expenses		Update Budget		Show Budget Overview";
+	
+	public String displayMenu(){
+		System.out.println(menu)
+	}
+	
+	public getMenuOption(String s){
+		super.
+	}
+	
 	public static void main(String[] args){
 	}
 }
