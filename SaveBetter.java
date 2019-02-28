8c5f2c5ff2ffd03aec6ce207e8d227aef5f580f1
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-02-28 14:41:12 -0700

    Add files via upload

--- SaveBetter.java
+++ SaveBetter.java
@@ -3,25 +3,24 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class SaveBetter extends UserData{
-	String menuList = "Update Income(0)		Update Expenses(1)		Update Amount to Save(2)		Show Budget Overview(3)";
-	List<String> menu = new ArrayList<String>();
-	String zero = "Update Income";
-	String one = "Update Expenses";
-	String two = "Update Amount to Save";
-	String three = "Show Budget Overview";
-	menu.add(zero);
-	menu.add(one);
-	menu.add(two);
-	menu.add(three);
-
+	String menuList = "Update Income		Update Expenses		Update Amount to Save		Show Budget Overview";
 	
 	public void displayMenu(){
 		System.out.println(menuList);
 	}
+
+	Scanner keyboard = new Scanner(System.in);
 	
-	public void getMenuOption(String s){
+	public void getMenuOption(String choice){
+		if (choice == "Update Income"){
+			System.out.print("Enter new Income: ");
+			double updatedIncome = keyboard.nextInt();
+			super.setMonthlyIncome(updatedIncome);
+			System.out.println("Your new income is: " + updatedIncome);
+		}
 	}
-	
+
 	public static void main(String[] args){
+		String hi = keyboard.nextLine();
 	}
 }
