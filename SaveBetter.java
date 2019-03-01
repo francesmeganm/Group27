d89c7d29f1b40ddb4363a9b13e85af65278e5971
kathrynl13 <47461692+kathrynl13@users.noreply.github.com>
2019-02-28 17:23:42 -0700

    Add files via upload

--- SaveBetter.java
+++ SaveBetter.java
@@ -5,28 +5,55 @@ import java.util.ArrayList;
 public class SaveBetter extends UserData{
 	String menuList = "Update Income		Update Expenses		Update Amount to Save		Show Budget Overview";
 	
-	public void displayMenu(){
-		System.out.println(menuList);
-	}
-
 	Scanner keyboard = new Scanner(System.in);
 	
-	public double getMenuOption(String choice){
-		if (choice == "Update Income"){
+	public double getMenuOption(double choice){
+		double returnStatement = 0.0;
+		if (choice == 0){
 			System.out.print("Enter new Income: ");
 			double updatedIncome = keyboard.nextInt();
 			super.setMonthlyIncome(updatedIncome);
 			System.out.println("Your new income is: " + updatedIncome);
-			return super.getMonthlyIncome();
+			returnStatement = super.getMonthlyIncome();
 		}
-		return 0.0;
+
+		if (choice == 1){
+			System.out.print("Enter new expenses: ");
+			double updatedExpenses = keyboard.nextInt();
+			super.setMonthlyExpenses(updatedExpenses);
+			System.out.println("Your new expenses is: " + updatedExpenses);
+			returnStatement = super.getMonthlyExpenses();
+		}
+
+		if (choice == 2){
+			System.out.print("Enter new amount to save: ");
+			double updatedAmountToSave = keyboard.nextInt();
+			super.setAmountToSave(updatedAmountToSave);
+			System.out.println("Your new amount to save is: " + updatedAmountToSave);
+			returnStatement = super.getAmountToSave();
+		}
+
+		/*
+		this one is hard because there is a good chance that budget overview has more than 
+		just number, and right now this method only returns double 
+
+		if (choice == 3){
+			System.out.print("This is your budget overview");
+
+		}*/
+		return returnStatement;
 	}
 
 	public static void main(String[] args){
 		SaveBetter sb = new SaveBetter();
 		Scanner keyboard = new Scanner(System.in);
+		System.out.println("To quite return 11");
 		System.out.println(sb.menuList);
-		String input = keyboard.nextLine();
-		sb.getMenuOption(input);
+		double input = keyboard.nextInt();
+
+		while (input != 11){
+			sb.getMenuOption(input);
+			input = keyboard.nextInt();
 		}
+	}
 }
