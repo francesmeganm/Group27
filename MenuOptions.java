5cfdc6b93bd76c93562b7660c43dd9f734b98e3f
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-02-28 17:35:45 -0700

    Delete MenuOptions.java

--- MenuOptions.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class MenuOptions extends UserData{
-	public static void main(String[] args){
-		HashMap<String,UserData> mc = new HashMap<String,UserData>();
-  		mc.put("Budget", UserData.getAmountToSave);
-	}
-  
-}
