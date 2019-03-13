d4ae2d14694137149cf5272516d9d48135279516
Koder741 <47461674+Koder741@users.noreply.github.com>
2019-03-13 17:44:42 -0600

    Calendar.java
    Note: User input is constructed according to the constructor provided by the  parent class (BudgetInfo). If there is anything else you would like to add to Calendar and display it, either update the constructor or add another one.

---
+++ Calendar.java
@@ -0,0 +1,42 @@
+import java.util.*
+
+                              
+
+public class Calendar extends BudgetInfo {
+
+	public static void main(String[] args) {
+	
+		BudgetInfo b = new BudgetInfo(); // Create a BudgetInfo object 
+		
+		BudgetInfo c = new Calendar(); // Create a Calendar object
+		
+		Scanner keyboard = new Scanner(System.in); // Create a Scanner object 
+		
+		System.out.println("Enter the Year you would like to create a budget for (YYYY): ");
+		int year = keyboard.nextLine(); // Read user input
+		
+		System.out.println("Enter the Month you would like to create a budget for (MM): ");
+		int month = keyboard.nextLine(); // Read user input
+		
+		System.out.println("Creating budget for:" + month + "/" + year ); // Message
+		
+		
+		// In this section get budget details by calling methods in the parent class, BudgetInfo
+		
+		double p = b.getPercentToSave();
+		double c = b.getGoalCost();
+		String g = b.getGoal();
+		
+		super(double p, double c, String g); 
+		
+		displayMonthlyBudget();
+	}
+		
+	public void displayMonthlyBudget() {	// Display the budget for the selected month
+		
+		System.out.println("Here is an overview of your budget for" + "" + month + "/" + year);
+	
+		System.out.println("Your Goal: " + g + "\n" + "Percent to Save: " + p + "\n" + "Goal Cost: " + c);
+		
+	}
+}
