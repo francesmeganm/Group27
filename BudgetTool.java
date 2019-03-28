ab4ddd026aa4c42c13a126cb9a1a92a67953189d
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-03-28 15:09:19 -0600

    Add files via upload

--- BudgetTool.java
+++ BudgetTool.java
@@ -12,33 +12,98 @@ public class BudgetTool{
 		user = new UserInfo();
 		smallBudget = new BudgetInfo(user);
 		largeBudget = new BudgetBreakdown(user);
-	
 	}
 
-	public void checkingUsername(username){
-		user.checkUsername(username);
-	}
+	public boolean checkingUsername(username){
+		boolean choice = user.checkUsername(username);
+    return choice;
+	} 
 
 	public void makeAccount(String username, String password){
 		//need to make a constructor to attach account and to user info, not sure
-		//how to do this but should not combine the two 
-
+		//how to do this but should not combine the two 	
 		account.createAccount(username, password);
 	}
 
-	public void login()
-
-
-	public boolean (String username, String password){
+	public boolean checkingLogin(String username, String password){
 		boolean verify = account.verifyUsernameAndPassword();
 		return verify;
 	}
 
-	public computeSmallBudget(){
-
-	}
-
-	public computeLargeBudget(){
-
-	}
+  public void updateUserInfo(double trans, double uti, double oth, double rent, double inc){
+    if (trans != 0){
+      user.setTrasport(trans);
+    }
+    if (uti != 0){
+      user.setUtility(uti);
+    }
+    if (oth != 0){
+      user.setOther(oth);
+    }
+    if (rent != 0){
+      user.setRentRent(rent);
+    }
+    if (inc != 0){
+      user.setMonthlyIncome(inc);
+    }
+  }
+  
+  public void month(String month){
+    //method choocse which month you are in
+  }
+  
+  public double gettingTheRemainingMoney(){
+    user.getRemainingMoney(remain);
+  }
+	
+  public double gettingTheAmountToSave(){
+    return user.getAmountToSave();
+  }
+  
+  public void settingTheAmountToSave(double save){
+    user.setAmountToSave(save);
+  }
+  
+  public Date gettingDateGoalCompleted(double goalCost){
+    return user.getDateGoalCompleted(goalCost);
+  }
+  
+  public void settingBudgetBreakdown(double ent, pers, food, shop, misc){
+    if (ent != 0){
+      user.setAmountForEntertainment(ent);
+    }
+    if (pers != 0){
+      user.setAmountForPersonalCare(pers);
+    }
+    if (food != 0){
+      user.getAmountForFoodAndGroceries(food);
+    }
+    if (shop != 0){
+      user.setAmountForShopping(shop);
+    }
+    if (misc != 0){
+      user.setAmountForMiscellaneous(misc);
+    }
+  }
+	
+  public double gettingEntertainment(){
+    return user.getAmountForEntertainment();
+  }
+  
+  public double gettingPersonal(){
+		return user.getAmountForPersonalCare();
+  }
+  
+  public double gettingFood(){
+    return user.getAmountForFoodAndGroceries();
+  }
+  
+  public double gettingShopping(){
+    return user.getAmountForShopping();
+  }
+  
+  public double gettingMisc(){
+    return user.getAmountForMiscellaneous();
+  }
+	
 }
