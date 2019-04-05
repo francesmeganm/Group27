9587a1db5e55b20d361fc5ea90fd58c65fea2f62
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-04-04 18:16:47 -0600

    Add files via upload

--- TextBudgetBreakdown.java
+++ TextBudgetBreakdown.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
 
+/** 
+* Class TextBudgetBreakdown refers to the "Create Budget Breakdown" option on the SaveBetter main menu.
+* This class displays to the user a breakdown of their remaining money to how they would like to spend it. 
+* It has one instance varibale, tool, which is a BudgetTool containing all the logic and methods behind each action.
+*/
 public class TextBudgetBreakdown{
 	BudgetTool tool;
 	Scanner keyboard = new Scanner(System.in);
 
+	/**
+	* This constructor takes a budgetTool and initializes it as the tool 
+	* @param budgetTool contains all the logic and methods behind each action the user may make
+	*/
 	public TextBudgetBreakdown(BudgetTool budgetTool){
 		this.tool = budgetTool;
 	}
 
+	/** 
+	* Method textBudgetBreakdown is what will be displayed to teh user when they choose
+	* to compute a budget breakdown. It will first prompt the user to enter percentages
+	* for each category, then it will compute the exact amount of your spending money that
+	* can be used for each category. 
+	*/
 	public void textBudgetBreakdown(){
 		System.out.println("\nThis is your complex budget breakdown.");
 		System.out.println("Based on these 5 spending categories, allocate a percentage of your spendings towards each category.");
