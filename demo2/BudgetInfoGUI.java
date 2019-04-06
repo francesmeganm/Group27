32512a6cb6c952e0589c5db169287aaf948704c0
candynwigwe <47461673+candynwigwe@users.noreply.github.com>
2019-04-06 15:48:41 -0600

    Add files via upload

--- demo2/BudgetInfoGUI.java
+++ demo2/BudgetInfoGUI.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import javafx.geometry.Pos;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import javafx.scene.text.Font;
 
 import java.util.Date;
 import java.util.Calendar;
@@ -17,6 +18,14 @@ import java.awt.Font.*;
 import javafx.scene.text.TextFlow.*;
 import javafx.scene.text.FontWeight;
 import java.lang.Object.*;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+
+import java.util.Locale;
 
 /**
 The class UserInfoGUI handles events of getting and setting variables that are in 
@@ -74,13 +83,16 @@ public class BudgetInfoGUI extends Application{
 		window.setTitle("SaveBetter");
 
 		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10,10,10,10));
-		grid.setVgap(10);
-		grid.setHgap(10);
+		grid.setPadding(new Insets(25,25,25,25));
+		grid.setVgap(25);
+		grid.setHgap(25);
+		final DatePicker datePicker = new DatePicker();
+        datePicker.localeProperty().set(Locale.US);
 
 		Label header = new Label("Quick Budgeting Analysis");
+		header.setFont(Font.font("Helvetica", FontWeight.BOLD, 25));
 		GridPane.setConstraints(header, 1, 0);
-		//header.setFont(Font.font("Helvetica", FontWeight.BOLD, 10));
+		
 		//setFont(Font.BOLD, header);
 		//String mg = "Quick Budgeting Analysis";
 		//header.setFont(Font.font("Times", BOLD, 12));
@@ -89,27 +101,32 @@ public class BudgetInfoGUI extends Application{
 
 		//AMOUNT AVALIABLE TO SAVE EACH MONTH
 		//BOLD
-		Label a = new Label("Avaliable funds to save each month");
+		Label a = new Label("Avaliable funds to save each month:");
+		a.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(a, 1, 1);
 		grid.getChildren().add(a);
 		//a.setStyle("-fx-font-weight: bold");
 
 		Label b = new Label("Monthly income less monthly expenses: ");
+		b.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(b, 1, 2);
 		grid.getChildren().add(b);
 
 		//BOLD
 		Label c = new Label(Double.toString(account.getRemainingMoney()));
+		c.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(c, 2, 2);
 		grid.getChildren().add(c);
 
 		//AMOUNT TO SAVE FROM PERCENTAGE
 		//BOLD
 		Label d = new Label("Amount of income to save a given percentage");
+		d.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(d, 1, 4);
 		grid.getChildren().add(d);
 
 		Label e = new Label("Desired percentage of income to save: ");
+		e.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(e, 1, 5);
 		grid.getChildren().add(e);
 
@@ -118,6 +135,7 @@ public class BudgetInfoGUI extends Application{
 		grid.getChildren().add(percentage);
 
 		Label f = new Label("The amount to save is: ");
+		f.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(f, 1, 6);
 		grid.getChildren().add(f);
 
@@ -134,10 +152,12 @@ public class BudgetInfoGUI extends Application{
 		//MONTHS UNTIL GOAL
 		//BOLD
 		Label i = new Label("Goal to save towards");
+		i.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(i, 1, 8);
 		grid.getChildren().add(i);
 
 		Label j = new Label("Cost of given goal: ");
+		j.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(j, 1, 9);
 		grid.getChildren().add(j);
 
@@ -151,9 +171,31 @@ public class BudgetInfoGUI extends Application{
 		k.setOnAction(new HandleGoal());
 
 		Label l = new Label("Date goal is completed: ");
+		l.setFont(Font.font ("Verdana", 15));
 		GridPane.setConstraints(l, 1, 10);
 		grid.getChildren().add(l);
 
+
+		HBox hBox = new HBox();
+        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+        hBox.setSpacing(10);
+		GridPane.setConstraints(hBox, 2, 10);
+       
+        hBox.getChildren().add(datePicker);
+		grid.getChildren().add(hBox);
+		
+		Date selected=datePicker.selectedDateProperty().get();
+
+        datePicker.selectedDateProperty().addListener(new InvalidationListener() {
+            @Override
+           public void invalidated(Observable observable) {
+            System.out.println(datePicker.selectedDateProperty().get());
+            }
+        });
+
+        datePicker.setLocale(Locale.US);
+        datePicker.getCalendarView().todayButtonTextProperty().set("Current Date");
+        //datePicker.getCalendarView().setShowWeeks(false);
 		//BOLD
 
 		Date t1 = new Date();
@@ -161,16 +203,21 @@ public class BudgetInfoGUI extends Application{
 		String date1 = format.format(t1);
 
 		Label input2 = new Label(format.format(t1));
-		GridPane.setConstraints(input2, 2, 10);
+		GridPane.setConstraints(input2, 2, 11);
 		grid.getChildren().add(input2);
 
 		Button back = new Button("Back to main menu");
-		GridPane.setConstraints(back, 3, 11);
+		GridPane.setConstraints(back, 3, 12);
 		grid.getChildren().add(back);
-		back.setOnAction(new HandleBackToMenu());
 		
-		Scene scene = new Scene(grid, 650, 400);
+		//grid.setSpacing(20);
+		grid.setAlignment(Pos.CENTER);
+		grid.setStyle("-fx-background-color: lightgreen;");
+		Scene scene = new Scene(grid, 1366, 768);
 		window.setScene(scene);
+		
+		scene.getStylesheets().add("calendarstyle.css");
+		
 
 		window.show();
 	}
