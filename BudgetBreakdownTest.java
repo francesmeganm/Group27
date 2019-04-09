3198efb29e8cf218882d548587a2c8660e8feedb
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-04-09 17:18:10 -0600

    Add files via upload

--- BudgetBreakdownTest.java
+++ BudgetBreakdownTest.java
@@ -1,16 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.io.*;
 
-public class BudgetBreakdownTest extends FormatTester{
+public class BudgetBreakdownTest{
 
 	public static final String CLASSNAME = "BudgetBreakdown";
 	public static final String FILENAME = CLASSNAME + ".java";
 
 	protected UserInfo u = new UserInfo(1000, 50, 50, 50, 50);
-
-	public BudgetBreakdownTest(){
-		super("BudgetBreakdown", false);
-	}
 
 	private void testInterface(){
 		String[] instanceVars = {"double amountForEntertainment", "double amountForPersonalCare", "double amountForFoodAndGroceries", 
@@ -117,13 +114,13 @@ public class BudgetBreakdownTest extends FormatTester{
 		bb.setAmountForShopping(50.0);
 		bb.setAmountExtra();
 
-		assertEquals("Unexpected extra money balance", 0.0, bb.getAmountExtra(), 0.00001);
+		assertEquals("Spending money fully distributed", 0.0, bb.getAmountExtra(), 0.00001);
 
 		bb.setAmountForMiscellaneous(25.0);
 		bb.setAmountForEntertainment(25.0);
 		bb.setAmountExtra();
 
-		assertEquals("Unexpected extra money balance", 0.0, bb.getAmountExtra(), 0.00001);
+		assertEquals("Spending money fully distributed", 0.0, bb.getAmountExtra(), 0.00001);
 	}
 
 	@Test
@@ -141,5 +138,91 @@ public class BudgetBreakdownTest extends FormatTester{
 
 		assertEquals("Unexpected extra money balance", 400.0, bb.getAmountExtra(), 0.00001);
 	}
+
+	@Test public void test_getAmountExtra_notUpdated(){
+		BudgetBreakdown bb = new BudgetBreakdown(u);
+
+		bb.setAmountForMiscellaneous(50.0);
+		bb.setAmountForShopping(25.0);
+
+		assertEquals("Amount extra was not updated", 0.0, bb.getAmountExtra(), 0.00001);
+
+		bb.setAmountExtra();
+		assertEquals("Amount extra should have updated", 200.0, bb.getAmountExtra(), 0.00001);
+	}
+
+	// END OF TESTS 
+
+	private boolean noDefaultConstructor(){
+		boolean noDefault = true;
+		String[] versions = new String[9];
+		versions[0] = "public " + CLASSNAME + "()";
+		versions[1] = "public " + CLASSNAME + " ()";
+		versions[2] = "public " + CLASSNAME + " ( )";
+		versions[3] = "protected " + CLASSNAME + "()";
+		versions[4] = "protected " + CLASSNAME + " ()";
+		versions[5] = "protected " + CLASSNAME + " ( )";
+		versions[6] = CLASSNAME + "()";
+		versions[7] = CLASSNAME + " ()";
+		versions[8] = CLASSNAME + " ( )";
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(FILENAME));
+			String line = in.readLine();
+			while (line != null) {
+				for (String stmt : versions) {
+					if (line.contains(stmt)) {
+						noDefault = false;
+					}
+				}
+				line = in.readLine();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			noDefault = false;
+		} catch (IOException e) {
+			noDefault =  false;
+		}
+		return noDefault;
+	}		
+
+	private boolean instanceVariablesArePrivate(String[] instanceVars){
+		boolean[] varsPrivate = new boolean[instanceVars.length];
+		for (int index = 0; index < instanceVars.length; index++){
+			varsPrivate[index] = false;
+		}
+		boolean allPrivate = true;
+
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(FILENAME));
+			String line = in.readLine();
+			while (line != null) {
+				if (line.contains("private")) {
+					line = line.trim();
+					for (int index = 0; index < instanceVars.length; index++){
+						String stmt = "private " + instanceVars[index];
+						if (line.length() >= stmt.length()){
+							String subline = line.substring(0,stmt.length());
+							if (subline.equals(stmt)){
+								varsPrivate[index] = true;
+							}
+						}
+					}
+				}
+				line = in.readLine();
+			}
+			in.close();
+		} catch (FileNotFoundException e) {
+			allPrivate = false;
+		} catch (IOException e) {
+			allPrivate =  false;
+		}
+
+		for (boolean b : varsPrivate) {
+			allPrivate = allPrivate && b;
+		}
+		return allPrivate;
+	}			
 
 }
